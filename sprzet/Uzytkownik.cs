cdd8a6e363c2876d0b1d359b367321f200f6dcd6
Karina Szczepanek <s26646@pjwstk.edu.pl>
2026-03-23 18:20:36 +0100

    klasa Uzytkownik zmodyfikowana

---
+++ sprzet/Uzytkownik.cs
@@ -0,0 +1,24 @@
+﻿namespace sprzet;
+
+public enum UserType
+{
+    Student,
+    Employee
+}
+
+public abstract class Uzytkownik
+{
+    public static int Count = 1;
+    UserType userType;
+    private string name, surname;
+    
+    public int Id { get; set; }
+    
+    public Uzytkownik(UserType userType, string name, string surname)
+    {
+        this.userType = userType;
+        this.name = name;
+        this.surname = surname;
+        Id = Count++;
+    }
+}
