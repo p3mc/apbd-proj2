40feeeb1a3e1112f390c26ae80f6501b1d30a720
Karina Szczepanek <s26646@pjwstk.edu.pl>
2026-03-23 18:34:18 +0100

    klasa Wypozyczenie

---
+++ sprzet/Wypozyczenie.cs
@@ -0,0 +1,19 @@
+﻿namespace sprzet;
+
+public class Wypozyczenie
+{
+    Uzytkownik uzytkownik;
+    Sprzet sprzet;
+    private bool ZwrotNaCzas { get; set; }
+    DateTime odData { get; set; }
+    int period { get; set; }
+
+    public Wypozyczenie(Uzytkownik uzytkownik, Sprzet sprzet, DateTime odData, int period)
+    {
+        this.uzytkownik = uzytkownik;
+        this.sprzet = sprzet;
+        this.odData = odData;
+        this.period = period;
+        ZwrotNaCzas = false;
+    }
+}
